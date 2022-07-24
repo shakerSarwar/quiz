e37f510ec4dc8d70f617e5848c4a2fcabd188dd4
shakerSarwar <shakersarwa4@gmail.com>
2022-07-24 14:31:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const questions = [{
+
+            "type": "single",
+            "question": "Which is not a step in the scientific method?",
+            "A" :'html',
+            "B" :'css',
+            "c" :'ja',
+            "D" :'html'
+        },
+        {
+
+            "type": "single",
+            "question": "Which is not a step in the scientific method?",
+            "A" :'html',
+            "B" :'css',
+            "c" :'ja',
+            "D" :'html'
+        },
+        {
+
+            "type": "single",
+            "question": "Which is not a step in the scientific method?",
+            "A" :'html',
+            "B" :'css',
+            "c" :'ja',
+            "D" :'html'
+        },
+        {
+
+            "type": "single",
+            "question": "Which is not a step in the scientific method?",
+            "A" :'html',
+            "B" :'css',
+            "c" :'ja',
+            "D" :'html'
+        },
+        {
+
+            "type": "single",
+            "question": "Which is not a step in the scientific method?",
+            "A" :'html',
+            "B" :'css',
+            "c" :'ja',
+            "D" :'html'
+        },
+        {
+
+            "type": "single",
+            "question": "Which is not a step in the scientific method?",
+            "A" :'html',
+            "B" :'css',
+            "c" :'ja',
+            "D" :'html'
+        }
+      
+        
+    ]
+
+
+const loadQuestion = () => {
+    const data = questions[index]
+    console.log(data)
+}
+
+loadQuestion();
